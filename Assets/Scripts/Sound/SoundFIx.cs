12110ea4f17a81c3282d403bde5df3db03f2a7f8
batomow <carlosa_miranda@outlook.com>
2017-08-12 17:47:12 -0500

    added audiosource object and put some script to fix the sound offset

---
+++ Assets/Scripts/Sound/SoundFIx.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFIx : MonoBehaviour {
+
+	private AudioSource _refComponent; 
+
+	void Awake(){
+		_refComponent = GetComponent<AudioSource> (); 
+	}
+
+	void Update(){
+		if (_refComponent.time == 13f) {
+			_refComponent.time = 0; 
+		}
+	}
+
+}
