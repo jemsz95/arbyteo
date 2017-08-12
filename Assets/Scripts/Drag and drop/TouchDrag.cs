8201bb685512b15df56019355dde0c0d9facc005
julio177 <julioc.aguilarv@gmail.com>
2017-08-12 00:07:37 -0500

    Touch and drag scripts

---
+++ Assets/Scripts/Drag and drop/TouchDrag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDrag : TouchSprite {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		funTouchInput(GetComponent<BoxCollider2D>());
+	}
+
+	void funOnFirstTouch() {
+		Vector3 pos;
+		Vector2 clickPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+		pos = new Vector3(clickPos.x, clickPos.y, transform.position.z);
+		transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/Drag and drop/TouchSprite.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchSprite : MonoBehaviour {
+
+	public static bool bGuiTouch = false;
+
+	public void funTouchInput(Collider2D collider) {
+		if(Input.touchCount > 0) {
+			if(collider == Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position))) {
+				switch(Input.GetTouch(0).phase) {
+					case TouchPhase.Began:
+						SendMessage("OnFirstTouchBegan", SendMessageOptions.DontRequireReceiver);
+						SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = true;
+						break;
+					case TouchPhase.Stationary:
+						SendMessage("OnFirstTouchStayed", SendMessageOptions.DontRequireReceiver);
+						SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = true;
+						break;
+					case TouchPhase.Moved:
+						SendMessage("OnFirstTouchMoved", SendMessageOptions.DontRequireReceiver);
+						SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = true;
+						break;
+					case TouchPhase.Ended:
+						SendMessage("OnFirstTouchEnded", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = false;
+						break;
+				}
+			}
+		}
+
+		if(Input.touchCount > 1) {
+			if(collider == Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.GetTouch(1).position))) {
+				switch(Input.GetTouch(1).phase) {
+					case TouchPhase.Began:
+						SendMessage("OnFirstTouchBegan", SendMessageOptions.DontRequireReceiver);
+						SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = true;
+						break;
+					case TouchPhase.Stationary:
+						SendMessage("OnFirstTouchStayed", SendMessageOptions.DontRequireReceiver);
+						SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = true;
+						break;
+					case TouchPhase.Moved:
+						SendMessage("OnFirstTouchMoved", SendMessageOptions.DontRequireReceiver);
+						SendMessage("OnFirstTouch", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = true;
+						break;
+					case TouchPhase.Ended:
+						SendMessage("OnFirstTouchEnded", SendMessageOptions.DontRequireReceiver);
+						bGuiTouch = false;
+						break;
+				}
+			}
+		}
+	}
+}
