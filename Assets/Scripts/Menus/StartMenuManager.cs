7c1e41a33fd592102db8f840c0fe096c09949329
julio177 <julioc.aguilarv@gmail.com>
2017-08-12 02:03:47 -0500

    Adds menu managers

---
+++ Assets/Scripts/Menus/StartMenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuManager : MonoBehaviour {
+
+	
+	public void funPlay() {
+		//SceneManager.LoadScene(2);
+	}
+
+	public void funCredits() {
+		SceneManager.LoadScene(1);
+	}
+
+	public void funExit() {
+		Application.Quit();
+	}
+}
