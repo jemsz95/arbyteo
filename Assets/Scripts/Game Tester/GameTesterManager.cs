55988975d2823d895e04467ec5fe1cd62e5907d2
batomow <carlosa_miranda@outlook.com>
2017-08-12 23:05:53 -0500

    added the new sprites and funcitonality

---
+++ Assets/Scripts/Game Tester/GameTesterManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameTesterManager : Singleton<GameTesterManager> {
+
+	private float _fLife, _fTime; 
+	public delegate void levelEvent (); 
+	public static event levelEvent GameTesterEnd; 
+
+	void Awake(){
+		_fLife = 100f; 
+		_fTime = 60f; 
+		GameTesterEnd += _funPassReferences; 
+	}
+
+	public void funAddTime(float famount){
+		_fTime += famount; 
+	}
+
+	public void funAddLife(float famount){
+		_fLife += famount; 
+	}
+
+	void Update(){
+		if (_fLife <= 0 || _fTime <= 0) {
+			GameTesterEnd (); 
+		}
+	}
+
+	void _funPassReferences(){
+		GameManager manager = GameObject.FindObjectOfType<GameManager> (); 
+		if(manager!=null){
+			//give the manager the references; 
+		}
+	}
+
+
+
+
+}
