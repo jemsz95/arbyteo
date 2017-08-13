e49e071e4530dd16106bd33519f58154047f578c
batomow <carlosa_miranda@outlook.com>
2017-08-13 09:47:17 -0500

    added sound to the credits

---
+++ Assets/Scripts/Sound/CreditsSoundFix.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsSoundFix : MonoBehaviour {
+
+
+	private AudioSource _refComponent; 
+
+	void Awake(){
+		_refComponent = GetComponent<AudioSource> (); 
+	}
+
+	void Update(){
+		if (_refComponent.time >= 8f) {
+			_refComponent.time = 0; 
+			_refComponent.Play (); 
+		}
+
+	}
+}
