fd2c2634e03f1063055afbbd042347326c7dd3eb
batomow <carlosa_miranda@outlook.com>
2017-08-13 00:24:50 -0500

    addid tutorial text

--- Assets/Scripts/Game Tester/GameTesterManager.cs
+++ Assets/Scripts/Game Tester/GameTesterManager.cs
@@ -6,16 +6,23 @@ using UnityEngine.UI;
 public class GameTesterManager : Singleton<GameTesterManager> {
 
 	private float _fLife, _fTime, _fScore; 
-	private Image _refImgTime, _refImgLife; 
+	private bool _bLevelStart; 
+	private Image _refImgTime, _refImgLife, _refTutorialText; 
 	public delegate void levelEvent (); 
 	public static event levelEvent GameTesterEnd; 
 
 	void Awake(){
 		_fLife = 100f; 
 		_fTime = 60f; 
+		_bLevelStart = false; 
 		GameTesterEnd += _funPassReferences; 
 		_refImgLife= GameObject.Find ("Life Fill").GetComponent<Image>(); 
 		_refImgTime = GameObject.Find ("Time Fill").GetComponent<Image> ();
+		_refTutorialText = GameObject.Find ("Tutorial Text").GetComponent<Image> (); 
+	}
+
+	void Start(){
+		StartCoroutine (Tutorial ()); 
 	}
 
 	public void funAddScore(float famount){
@@ -25,7 +32,7 @@ public class GameTesterManager : Singleton<GameTesterManager> {
 	}
 
 	public void funAddLife(float famount){
-		if (_fLife > 1) {
+		if (_fLife > 1 && _bLevelStart) {
 			_fLife = Mathf.Clamp (_fLife += famount, 0, 100); 
 		}
 	}
@@ -49,6 +56,19 @@ public class GameTesterManager : Singleton<GameTesterManager> {
 		} else {
 			Debug.Log (_fScore); 
 		}
+	}
+
+	IEnumerator Tutorial(){
+		while (true) {
+
+			if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.KeypadEnter)){
+				break; 
+			}else{
+				_refTutorialText.enabled = true; 
+			}
+			yield return new WaitForSeconds (Time.deltaTime); 
+		}
+		_refTutorialText.enabled = false; 
 	}
 
 
