09680d7fdbe5552aeefc3adfa1a4a8f124defbcc
batomow <carlosa_miranda@outlook.com>
2017-08-13 13:55:54 -0500

    added music to the title screen

---
+++ Assets/Scripts/Sound/TItleScreenMusicFix.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TItleScreenMusicFix : MonoBehaviour {
+
+	private AudioSource _refComponent; 
+
+	void Awake(){
+		_refComponent = GetComponent<AudioSource> (); 
+	}
+
+	void Update(){
+		if (_refComponent.time >= 12f) {
+			_refComponent.time = 0; 
+			_refComponent.Play (); 
+		}
+
+	}
+}
