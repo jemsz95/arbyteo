180fa0bac67a871829718da0dad08596f333df0c
batomow <carlosa_miranda@outlook.com>
2017-08-13 15:35:25 -0500

    dfa;lksdjaf;

---
+++ Assets/Scripts/Sound/PlayClickSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayClickSound : MonoBehaviour {
+
+	private AudioSource me; 
+
+	void Awake(){
+		me = this.gameObject.GetComponent<AudioSource> (); 
+	}
+
+	 public void clicked (){
+		me.Play (); 
+	}
+
+
+}
